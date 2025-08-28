ca856dcd26fda3fc66b077205448ed6a543933bf
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2025-08-28 12:49:49 +0200

    Add a cheesy line in the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,19 @@
+import { Keyboard } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+
 const Footer = () => {
   return (
     <footer className="text-muted-foreground py-5 text-center">
       Andrej Zafirovski © {new Date().getFullYear()}
+      <div className="flex items-center gap-x-1 justify-center">
+        <div className="text-xs">Made with </div>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Keyboard size={18} />
+          </TooltipTrigger>
+          <TooltipContent>Keyboard</TooltipContent>
+        </Tooltip>
+      </div>
     </footer>
   );
 };
