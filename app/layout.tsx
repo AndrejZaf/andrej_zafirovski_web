e35e2b4a7e38a5d067d2f57f141640e4e2a36d63
AndrejZaf <andrej.zafirovski@students.finki.ukim.mk>
2025-08-29 09:50:00 +0200

    Add an og.jpg to override the default image

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,25 @@ export const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Andrej Zafirovski",
   description: "Welcome to my portfolio",
+  openGraph: {
+    type: "website",
+    url: "/",
+    siteName: "andrejzafirovski.com",
+    images: [
+      {
+        url: "/og.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Andrej Zafirovski - Portfolio",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Andrej Zafirovski - Portfolio",
+    description: "Software Engineer - Projects & Notes",
+    images: ["/og.jpg"],
+  },
 };
 
 export default function RootLayout({
